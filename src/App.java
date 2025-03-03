f464193ccba66723e697cbfc73346a8a78623f7d
UmutAltintas <umutaltintas2008@.com>
2025-03-03 23:53:13 +0300

    Changed Util class to Utils, tested if the files are working.

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        String sampleText = "This is a sample text to check for AI generation.";
+        System.out.println(sampleText);
+    }
+}
