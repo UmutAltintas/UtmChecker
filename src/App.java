6216bfdcc7318887458b3fa6c128226f6d230348
UmutAltintas <umutaltintas2008@.com>
2025-03-04 11:09:42 +0300

    Changed Files, turned to Python.

--- src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        String sampleText = "This is a sample text to check for AI generation.";
-        System.out.println(sampleText);
-    }
-}
